803228620f01c3deffb68b1bf6fdd526db6142ee
pm2828 <pramodmandlik28@gmail.com>
2024-11-10 20:07:12 +0530

    tech-master-compass

--- src/components/Compass.js
+++ src/components/Compass.js
@@ -12,31 +12,10 @@ function styleText(text) {
   });
 }
 
-// function Compass() {
-//   return (
-//     <Container fluid className="compass min-vh-100">
-//       <Row className="align-items-center min-vh-100">
-//         {/* Left Half: Image */}
-//         <Col md={6} className="compass-image-container d-flex justify-content-center align-items-center">
-//           <img className="compass-icon" src="/images/compass3.jpg" alt="Compass Icon" />
-//         </Col>
-
-//         {/* Right Half: Text Content */}
-//         <Col md={6} className="compass-text">
-//           <h2>{styleText('TECH M ASTER COMPASS')}</h2>
-//           <p>{styleText('Tech M aster Compass is your go-to platform, providing a comprehensive suite of insights from Dashboards, Application Health Checks, and Automation Statistics.')}</p>
-//           <p>{styleText('It also offers Tech M aster Compass self-help tools to search historical Production Support incidents, ensuring efficient management and resolution of current incidents.')}</p>
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// }
-
-
- function Compass() {
+function Compass() {
   return (
-    <Container fluid className="compass-container vh-100">
-      <Row className="compass-row align-items-center min-s-100">
+    <Container fluid className="compass min-vh-100">
+      <Row className="align-items-center min-vh-100">
         {/* Left Half: Image */}
         <Col md={6} className="compass-image-container d-flex justify-content-center align-items-center">
           <img className="compass-icon" src="/images/compass3.jpg" alt="Compass Icon" />
@@ -44,13 +23,9 @@ function styleText(text) {
 
         {/* Right Half: Text Content */}
         <Col md={6} className="compass-text">
-          <h2 className="techmaster-heading">TECHMASTER COMPASS</h2>
-          <p className="compass-description">
-            TechMaster Compass is your go-to platform, providing a comprehensive suite of insights from Dashboards, Application Health Checks, and Automation Statistics.
-          </p>
-          <p className="compass-description">
-            It also offers TechMaster Compass self-help tools to search historical Production Support Incidents.
-          </p>
+          <h2 className="techmaster-heading">{styleText('Tech M aster Compass')}</h2>
+          <p>{styleText('Tech M aster Compass is your go-to platform, providing a comprehensive suite of insights from Dashboards, Application Health Checks, and Automation Statistics.')}</p>
+          <p>{styleText('It also offers Tech M aster Compass self-help tools to search historical Production Support incidents, ensuring efficient management and resolution of current incidents.')}</p>
         </Col>
       </Row>
     </Container>
